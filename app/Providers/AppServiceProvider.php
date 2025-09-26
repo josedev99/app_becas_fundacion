bda7ddb985899b0a915768299dafba0f1618b129
jose dev <josededanes99@gmail.com>
2025-09-25 21:40:47 -0600

    Update...Tunnel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //Temporal
+        if (app()->environment(['production','local'])) {
+            URL::forceScheme('https');
+        }
     }
 }
